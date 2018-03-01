7ba084214377d28bf89f9c44e06d0a3094928b30
Kush Patel <KushPatel@nbp-219-111.nbp.ruw.rutgers.edu>
2018-03-01 10:11:12 -0500

    Added Queen file, Kush first commit

---
+++ src/objects/Queen.java
@@ -0,0 +1,30 @@
+package objects;
+
+public class Queen extends Piece {
+	// int x
+	// int y
+	// boolean firstmove = false;
+	// boolean team;
+	// boolean alive = true;
+	
+	public Queen(int x, int y, boolean team) {
+		this.x = x;
+		this.y = y;
+		
+		this.team = team;
+		firstmove = true;
+	}
+
+	@Override
+	public void move(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static Piece Queen(int x, int y, boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
